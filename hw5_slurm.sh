cefc1c53270a147bf982f66d7333451f021e97e4
Nicholas McCarty <mccartnp@pitzer-rw02.ten.osc.edu>
2024-10-16 12:10:20 -0400

    Test commit

--- hw5_slurm.sh
+++ hw5_slurm.sh
@@ -19,14 +19,14 @@ g++ -g -Wall -std=c++17 -O3 Matrix.cpp NeuralNet.cpp main.cpp -o homework5
 unzip -q /fs/ess/PMIU0184/cse443/data/mnist_images.zip -d "${TMPDIR}"
 
 # Uncomment the following for profiling 
-# perf record -F 20 --call-graph dwarf ./homework5 "${TMPDIR}/data"
+perf record -F 20 --call-graph dwarf ./homework5 "${TMPDIR}/data"
 
 # Comment out the time lines when profiling.
 # Run the program 5 times to get measure consistent timings
-/usr/bin/time -v ./homework5 "${TMPDIR}/data"
-/usr/bin/time -v ./homework5 "${TMPDIR}/data"
-/usr/bin/time -v ./homework5 "${TMPDIR}/data"
-/usr/bin/time -v ./homework5 "${TMPDIR}/data"
-/usr/bin/time -v ./homework5 "${TMPDIR}/data"
+# /usr/bin/time -v ./homework5 "${TMPDIR}/data"
+# /usr/bin/time -v ./homework5 "${TMPDIR}/data"
+# /usr/bin/time -v ./homework5 "${TMPDIR}/data"
+# /usr/bin/time -v ./homework5 "${TMPDIR}/data"
+# /usr/bin/time -v ./homework5 "${TMPDIR}/data"
 
 #end of script
